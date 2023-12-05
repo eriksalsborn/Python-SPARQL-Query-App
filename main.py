5c0854539356bd4d827863a4bfc9c7343895ecda
Erik Salsborn <erik.salsborn@hotmail.com>
2023-12-05 16:28:31 +0100

    Fix query-bug for fetching actors + Add OPTIONAL tag

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def query_dbpedia(query, sparql_endpoint):
 def display_results(results, name_input):
     if results:
         results_df = pd.json_normalize(results)
-        display_df = results_df[['filmName.value', 'directorName.value','allActorsName.value', 'gross.value', 'distributorName.value', 'durationInMinutes.value']]
-        display_df.columns = ['Film', 'Director' ,'Actors', 'Gross', 'Distributor', 'Duration (minutes)']
+        display_df = results_df[['filmName.value', 'directorName.value', 'allActorsName.value', 'durationInMinutes.value', 'gross.value', 'distributorName.value']]
+        display_df.columns = ['Film', 'Director', 'Actors', 'Duration (min)', 'Gross (USD)', 'Distributor']
         print(display_df)
     else:
         print(f"No movies found for: {name_input}")
--- query.py
+++ query.py
@@ -5,31 +5,43 @@ def get_movies_by_director(director_name):
         PREFIX dbp: <http://dbpedia.org/property/>
         PREFIX dbr: <http://dbpedia.org/resource/>
         PREFIX dbo: <http://dbpedia.org/ontology/>
-        
-        SELECT ?filmName, 
-        (GROUP_CONCAT(DISTINCT ?directorName; separator=",") AS ?directorName),
-        (GROUP_CONCAT(DISTINCT ?allActorsName; separator=",") AS ?allActorsName),
-        (GROUP_CONCAT(DISTINCT ?distributorName; separator=",") AS ?distributorName),
-        ?gross,
-        ?durationInMinutes
-        WHERE {{
-            ?film dbo:gross ?grossUri ;
-                dbo:distributor ?distributor ;
-                dbo:runtime ?runtime ;
-                dbp:name ?filmName ;
-                dbo:starring ?allActors ;
-                dbo:director ?director .
-    
-            ?distributor dbp:name ?distributorName .
-            ?allActors dbp:name ?allActorsName .
-            ?director dbp:name ?directorName .
-    
-            BIND(xsd:decimal(?runtime) / 60 AS ?durationInMinutes)
-            BIND(xsd:decimal(?grossUri) AS ?gross)
 
-            FILTER(regex(?director, "{format_input(director_name)}", "i"))
+        SELECT ?filmName, 
+            (GROUP_CONCAT(DISTINCT ?directorName; separator=" | ") AS ?directorName),
+            (GROUP_CONCAT(DISTINCT ?allActorsName; separator=" | ") AS ?allActorsName),
+            (MAX(?durationInMinutes) AS ?durationInMinutes),
+            (MAX(?gross) AS ?gross),
+            (MAX(?distributorName) AS ?distributorName)
+        WHERE {{
+            ?film dbp:name ?filmName .
+
+            OPTIONAL {{ 
+                ?film dbo:director ?director . 
+                ?director dbp:name ?directorName .
+            }}
+
+            OPTIONAL {{ 
+                ?film dbp:starring ?allActorsName .
+            }}
+
+            OPTIONAL {{ 
+                ?film dbo:distributor ?distributor . 
+                ?distributor dbp:name ?distributorName .
+            }}
+
+            OPTIONAL {{ 
+                ?film dbo:runtime ?runtime .
+                BIND(xsd:decimal(?runtime) / 60 AS ?durationInMinutes)
+            }}
+
+            OPTIONAL {{
+                ?film dbo:gross ?grossUri . 
+                BIND(xsd:decimal(?grossUri) AS ?gross)
+            }}
+
+            FILTER(regex(?directorName, "{director_name}", "i"))
         }}
-        GROUP BY ?filmName ?gross ?durationInMinutes
+        GROUP BY ?filmName
         ORDER BY DESC(?gross)
     """
 
@@ -39,64 +51,92 @@ def get_movies_by_actor(actor_name):
         PREFIX dbp: <http://dbpedia.org/property/>
         PREFIX dbr: <http://dbpedia.org/resource/>
         PREFIX dbo: <http://dbpedia.org/ontology/>
-        
-        SELECT ?filmName, 
-        (GROUP_CONCAT(DISTINCT ?directorName; separator=",") AS ?directorName),
-        (GROUP_CONCAT(DISTINCT ?allActorsName; separator=",") AS ?allActorsName),
-        (GROUP_CONCAT(DISTINCT ?distributorName; separator=",") AS ?distributorName),
-        ?gross,
-        ?durationInMinutes
-        WHERE {{
-            ?film dbo:gross ?grossUri ;
-                dbo:distributor ?distributor ;
-                dbo:runtime ?runtime ;
-                dbp:name ?filmName ;
-                dbp:starring ?allActors ;
-                dbo:director ?director .
-    
-            ?distributor dbp:name ?distributorName .
-            ?allActors dbp:name ?allActorsName .
-            ?director dbp:name ?directorName .
-    
-            BIND(xsd:decimal(?runtime) / 60 AS ?durationInMinutes)
-            BIND(xsd:decimal(?grossUri) AS ?gross)
 
-            FILTER(regex(?allActors, "{format_input(actor_name)}", "i"))
+        SELECT ?filmName, 
+            (GROUP_CONCAT(DISTINCT ?directorName; separator=" | ") AS ?directorName),
+            (GROUP_CONCAT(DISTINCT ?allActorsName; separator=" | ") AS ?allActorsName),
+            (MAX(?durationInMinutes) AS ?durationInMinutes),
+            (MAX(?gross) AS ?gross),
+            (MAX(?distributorName) AS ?distributorName)
+        WHERE {{
+            ?film dbp:name ?filmName .
+
+            OPTIONAL {{ 
+                ?film dbo:director ?director . 
+                ?director dbp:name ?directorName .
+            }}
+
+            OPTIONAL {{ 
+                ?film dbp:starring ?allActorsName .
+                ?film dbp:starring ?searchActor .
+            }}
+
+            OPTIONAL {{ 
+                ?film dbo:distributor ?distributor . 
+                ?distributor dbp:name ?distributorName .
+            }}
+
+            OPTIONAL {{ 
+                ?film dbo:runtime ?runtime .
+                BIND(xsd:decimal(?runtime) / 60 AS ?durationInMinutes)
+            }}
+
+            OPTIONAL {{
+                ?film dbo:gross ?grossUri . 
+                BIND(xsd:decimal(?grossUri) AS ?gross)
+            }}
+
+            FILTER(regex(?searchActor, "{actor_name}", "i"))
         }}
-        GROUP BY ?filmName ?gross ?durationInMinutes
+        GROUP BY ?filmName
         ORDER BY DESC(?gross)
     """
 
 def get_info_about_movie(movie_name):
     return f"""
         PREFIX dbp: <http://dbpedia.org/property/>
+        PREFIX dbr: <http://dbpedia.org/resource/>
         PREFIX dbo: <http://dbpedia.org/ontology/>
 
         SELECT ?filmName, 
-            (GROUP_CONCAT(DISTINCT ?directorName; separator=",") AS ?directorName),
-            (GROUP_CONCAT(DISTINCT ?allActorsName; separator=",") AS ?allActorsName),
-            (GROUP_CONCAT(DISTINCT ?distributorName; separator=",") AS ?distributorName),
-            ?gross,
-            ?durationInMinutes
+            (GROUP_CONCAT(DISTINCT ?directorName; separator=" | ") AS ?directorName),
+            (GROUP_CONCAT(DISTINCT ?allActorsName; separator=" | ") AS ?allActorsName),
+            (MAX(?durationInMinutes) AS ?durationInMinutes),
+            (MAX(?gross) AS ?gross),
+            (MAX(?distributorName) AS ?distributorName)
         WHERE {{
-            ?film dbo:gross ?grossUri ;
-                dbo:distributor ?distributor ;
-                dbo:runtime ?runtime ;
-                dbp:name ?filmName ;
-                dbp:starring ?allActors ;
-                dbo:director ?director .
+            ?film dbp:name ?filmName .
 
-            ?distributor dbp:name ?distributorName .
-            ?allActors dbp:name ?allActorsName .
-            ?director dbp:name ?directorName .
+            OPTIONAL {{ 
+                ?film dbo:director ?director . 
+                ?director dbp:name ?directorName .
+            }}
 
-            BIND(xsd:decimal(?runtime) / 60 AS ?durationInMinutes)
-            BIND(xsd:decimal(?grossUri) AS ?gross)
+            OPTIONAL {{ 
+                ?film dbp:starring ?allActorsName .
+            }}
+
+            OPTIONAL {{ 
+                ?film dbo:distributor ?distributor . 
+                ?distributor dbp:name ?distributorName .
+            }}
+
+            OPTIONAL {{ 
+                ?film dbo:runtime ?runtime .
+                BIND(xsd:decimal(?runtime) / 60 AS ?durationInMinutes)
+            }}
+
+            OPTIONAL {{
+                ?film dbo:gross ?grossUri . 
+                BIND(xsd:decimal(?grossUri) AS ?gross)
+            }}
 
             FILTER(regex(?filmName, "{movie_name}", "i"))
         }}
-        GROUP BY ?filmName ?gross ?durationInMinutes
+        GROUP BY ?filmName
         ORDER BY DESC(?gross)
     """
+
+
 
 
